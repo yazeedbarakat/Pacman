43d22d7af4a05354a69ed3c4361efb4a1ab9537a
Yazeed <yazeedbarakat731@gmail.com>
2026-07-22 23:59:32 +0300

    nd

--- maze.py
+++ maze.py
@@ -36,3 +36,37 @@ class Player:
         self.position = self.center
         return self.lives > 0
 
+
+class Pacgum:
+    def __init__(self, position: tuple[int, int]) -> None:
+        self.position: tuple[int, int] = position
+        self.eaten: bool = False
+        self.points = 10
+
+    def eat(self) -> int:
+        self.eaten = True
+        return self.points
+
+class SuperPacgum(Pacgum):
+    def __init__(self, position: tuple[int, int]) -> None:
+        super().__init__(self.position)
+        self.points = 50
+
+
+
+def place_pacgums(maze_size: tuple[int, int], maze_grid: list[list[int]]) -> list[Pacgum]:
+    pacgums: list[Pacgum, SuperPacgum] = []
+    corners: list[tuple[int, int]] = [
+            (0, 0), (maze_size[0] -1, 0), (0, maze_size[1] -1),
+            (maze_size[0] -1, maze_size[1] -1)
+            ]
+    for row in range(maze_size[1]):
+        for col in range(maze_size[0]):
+            if maze_grid[row][col] != 0xF and not (col, row) in corners:
+                pacgums.append(Pacgum((col, row)))
+    for cor in corners:
+        pacgums.append(SuperPacgum(cor))
+    return pacgums
+
+
+
